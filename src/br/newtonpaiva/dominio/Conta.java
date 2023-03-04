05d55d46dc625dc68c6495a6c019295a8c8e4f78
Tarley Lana <tarley.lana@gmail.com>
2023-03-03 21:20:49 -0300

    feat: implementa construtores, equals e hashcode

--- src/br/newtonpaiva/dominio/Conta.java
+++ src/br/newtonpaiva/dominio/Conta.java
@@ -1,9 +1,24 @@
 
 package br.newtonpaiva.dominio;
 
+import java.util.Objects;
+
 public class Conta {
     private Integer numero;
     private Double saldo;
+
+    public Conta() {
+        this(null);
+    }
+
+    public Conta(Integer numero) {
+        this(numero, 0.0);
+    }
+
+    public Conta(Integer numero, Double saldo) {
+        this.numero = numero;
+        this.saldo = saldo;
+    }
 
     public Double sacar(Double valor) {
         return 0.0;
@@ -31,6 +46,19 @@ public class Conta {
 
     public void setSaldo(Double saldo) {
         this.saldo = saldo;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Conta conta = (Conta) o;
+        return Objects.equals(numero, conta.numero);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(numero);
     }
 }
 
