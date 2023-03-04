31f731af6bbb32e9b663e83221e24587bd9c9eae
Tarley Lana <tarley.lana@gmail.com>
2023-03-03 21:22:51 -0300

    feat: usa o equal para comparar as contas

--- src/br/newtonpaiva/ui/Main.java
+++ src/br/newtonpaiva/ui/Main.java
@@ -4,12 +4,24 @@ import br.newtonpaiva.dominio.Conta;
 
 public class Main {
     public static void main(String[] args) {
-        Conta c = new Conta();
-        c.setNumero(10);
+        Conta c = new Conta(10);
+//        c.setNumero(10);
         c.setSaldo(100.0);
 
         c.depositar(50.0);
-        c.depositar(-20.0);
+//        c.depositar(-20.0);
+
+        Conta c2 = new Conta();
+        c2.setNumero(10);
+        c2.setSaldo(500.0);
+
+        Conta c3 = new Conta();
+
+        if (c.equals(c2)) {
+            System.out.println("Mesma conta");
+        } else {
+            System.out.println("Contas diferentes");
+        }
 
         System.out.println(c.getSaldo());
     }
